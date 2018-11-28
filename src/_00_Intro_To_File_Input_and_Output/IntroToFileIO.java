4a4bc71757571c1429ccf0cbb85842a132aad5f3
ryan.palmer <ryan.palmer@servicenow.com>
2018-11-27 19:23:55 -0800

    Fixed incorrect file paths in IntroToFileIO.java. Added the missing test.txt file which is required for this example to work.

--- src/_00_Intro_To_File_Input_and_Output/IntroToFileIO.java
+++ src/_00_Intro_To_File_Input_and_Output/IntroToFileIO.java
@@ -12,12 +12,11 @@ public class IntroToFileIO {
 		
 		//Read from a file one character at a time
 		try {
-			FileReader fr = new FileReader("src/intro_to_file_io/test.txt");
+			FileReader fr = new FileReader("src/_00_Intro_To_File_Input_and_Output/test.txt");
 			int c = fr.read();
 			while(c != -1){
 				System.out.print((char)c);
 				c = fr.read();
-				
 			}
 			fr.close();
 		} catch (FileNotFoundException e) {
@@ -28,7 +27,7 @@ public class IntroToFileIO {
 		
 		//Read from a file one line at a time
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("src/intro_to_file_io/test.txt"));
+			BufferedReader br = new BufferedReader(new FileReader("src/_00_Intro_To_File_Input_and_Output/test.txt"));
 			
 			String line = br.readLine();
 			while(line != null){
@@ -47,12 +46,12 @@ public class IntroToFileIO {
 
 		//Write to a file
 		try {
-			FileWriter fw = new FileWriter("src/intro_to_file_io/test2.txt");
+			FileWriter fw = new FileWriter("src/_00_Intro_To_File_Input_and_Output/test2.txt");
 			
 			/*
 			NOTE: To append to a file that already exists, add true as a second parameter when calling the
 			      FileWriter constructor.
-			      (e.g. FileWriter fw = new FileWriter("src/intro_to_file_io/test2.txt", true);)
+			      (e.g. FileWriter fw = new FileWriter("src/_00_Intro_To_File_Input_and_Output/test2.txt", true);)
 			*/
 			
 			fw.write("\nThis is me writing a message");

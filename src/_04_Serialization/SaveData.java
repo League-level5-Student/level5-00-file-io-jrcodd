00918236c1fc7daa2219a8203ac82b829d6e9492
ryan.palmer <ryan.palmer@servicenow.com>
2018-11-27 20:21:58 -0800

    Added new Serialization assignment that introduces students to simple file IO with serializable classes.

---
+++ src/_04_Serialization/SaveData.java
@@ -0,0 +1,14 @@
+package _04_Serialization;
+
+/*
+ * Complete this class so that it can be serialized.
+ */
+public class SaveData {
+	public final String name;
+	public final int age;
+
+	public SaveData(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}

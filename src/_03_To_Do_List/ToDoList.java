adf8d2a858f2a6c6e006811bfdcf4917ba0578c6
league <league@imac-27.attlocal.net>
2020-01-28 16:59:14 -0800

    todolist

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -4,10 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -32,24 +35,24 @@ public class ToDoList implements ActionListener {
 	 * When the program starts, it should automatically load the last saved file
 	 * into the list.
 	 */
+	JFrame f = new JFrame();
+	JPanel p = new JPanel();
+	JButton add = new JButton();
+	JButton remove = new JButton();
+	JButton save = new JButton();
+	JButton load = new JButton();
+	JButton view = new JButton();
+	ArrayList<String> todoList = new ArrayList<String>();
+	String temp = "";
+
 	public static void main(String[] args) {
+
 		ToDoList tdl = new ToDoList();
 		tdl.startup();
 	}
 
 	public void startup() {
-		
-		JFrame f = new JFrame();
-		JPanel p = new JPanel();
-		JButton add = new JButton();
-		JButton remove = new JButton();
-		JButton save = new JButton();
-		JButton load = new JButton();
-		JButton view = new JButton();
-		JTextField stream = new JTextField();
-		
 		f.add(p);
-		p.add(stream);
 		p.add(add);
 		p.add(remove);
 		p.add(view);
@@ -58,21 +61,68 @@ public class ToDoList implements ActionListener {
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		add.addActionListener(this);
+		add.setText("add");
+		remove.addActionListener(this);
+		remove.setText("remove");
+		save.addActionListener(this);
+		save.setText("save");
+		load.addActionListener(this);
+		load.setText("load");
+		view.addActionListener(this);
+		view.setText("view");
 		f.setSize(500, 500);
-		stream.setEditable(false);
-		try {
-			BufferedReader fr = new BufferedReader(new FileReader("src/_03_To_Do_List/stream.txt"));
-			stream.setSize(100,  100);
-			stream.setText(fr.readLine());
 		
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("click");
+		if (e.getSource() == add) {
+			FileWriter fw = null;
+			String Ainput = JOptionPane.showInputDialog("What do you want to add to your to-do list?");
+			try {
+				fw = new FileWriter("src/_03_To_Do_List/stream.txt", true);
+				fw.write("~     " + Ainput + "\n");
+			} catch (IOException e1) {
+				e1.printStackTrace();
+
+			} finally {
+				try {
+					fw.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+
+			}
+		}
+		if (e.getSource() == remove) {
+			String Rinput = JOptionPane.showInputDialog("What do you want to remove from your to-do list?");
+			todoList.remove(Rinput);
+		}
+
+		if (e.getSource() == (save)) {
+
+		}
+		if (e.getSource() == (view)) {
+
+			BufferedReader br = null;
+			try {
+				temp = " ";
+				br = new BufferedReader(new FileReader("src/_03_To_Do_List/stream.txt"));
+				while (br.readLine() != null) {
+					temp += (br.readLine());
+				}
+
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			} finally {
+				try {
+					br.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+		}
 	}
 }

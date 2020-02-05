98fd6c7c042fca03037d7063ba094d4339dae996
league <league@imac-27.attlocal.net>
2020-02-04 16:56:43 -0800

    finished todoList and serializable

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -3,6 +3,7 @@ package _03_To_Do_List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -71,57 +72,63 @@ public class ToDoList implements ActionListener {
 		view.addActionListener(this);
 		view.setText("view");
 		f.setSize(500, 500);
-		
+
 	}
+
+	String introFormat = "-  ";
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == add) {
-			FileWriter fw = null;
 			String Ainput = JOptionPane.showInputDialog("What do you want to add to your to-do list?");
-			try {
-				fw = new FileWriter("src/_03_To_Do_List/stream.txt", true);
-				fw.write("~     " + Ainput + "\n");
-			} catch (IOException e1) {
-				e1.printStackTrace();
-
-			} finally {
-				try {
-					fw.close();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-
-			}
+			todoList.add(introFormat + Ainput + "\n");
 		}
 		if (e.getSource() == remove) {
 			String Rinput = JOptionPane.showInputDialog("What do you want to remove from your to-do list?");
-			todoList.remove(Rinput);
+			todoList.remove(introFormat + Rinput + "\n");
 		}
 
 		if (e.getSource() == (save)) {
+			try {
+				FileWriter fw = new FileWriter("src/_03_To_Do_List/stream.txt");
+				for (String input : todoList) {
 
+					fw.write(input + "\n");
+				}
+				fw.close();
+			} catch (FileNotFoundException e2) {
+				e2.printStackTrace();
+			} catch (IOException e3) {
+				e3.printStackTrace();
+			}
 		}
 		if (e.getSource() == (view)) {
+			temp = "";
+			if (todoList.size() > 0) {
+				for (int i = 0; i < todoList.size(); i++) {
+					if (!(temp.contains(todoList.get(i)))) {
+						temp += (todoList.get(i));
+					}
 
-			BufferedReader br = null;
+				}
+			}
+			JOptionPane.showMessageDialog(null, temp);
+		}
+		if (e.getSource() == (load)) {
+
+			todoList.clear();
 			try {
-				temp = " ";
-				br = new BufferedReader(new FileReader("src/_03_To_Do_List/stream.txt"));
-				while (br.readLine() != null) {
-					temp += (br.readLine());
-				}
+				BufferedReader br = new BufferedReader(new FileReader("src/_03_To_Do_List/stream.txt"));
+				String str = br.readLine();
+				do {
+					System.out.println(str);
+					todoList.add(str + "\n");
+					str = br.readLine();
+				} while (!(str == null));
 
-			} catch (IOException e2) {
-				e2.printStackTrace();
-			} finally {
-				try {
-					br.close();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-
+				br.close();
+			} catch (IOException e4) {
+				e4.printStackTrace();
 			}
 		}
 	}
--- src/_04_Serialization/SaveData.java
+++ src/_04_Serialization/SaveData.java
@@ -1,9 +1,16 @@
 package _04_Serialization;
 
+import java.io.Serializable;
+
 /*
  * Complete this class so that it can be serialized.
  */
-public class SaveData {
+public class SaveData implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	public final String name;
 	public final int age;
 
